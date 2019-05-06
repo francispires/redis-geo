e01bf6e275c2143ca95f7da3370ef6bf34b634be
Francis Pires de Carvalho <francisp@LFP.LOCAL>
2019-05-06 17:55:04 -0300

    teste

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set environment variables used in deploy.sh and AWS task-definition.json:
-export IMAGE_NAME=770676894615.dkr.ecr.us-east-2.amazonaws.com/redis-geo:latest
+export IMAGE_NAME=redis-geo:latest
 export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-east-2
