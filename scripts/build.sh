f1c87903b0ab11d82f194b0acfd8f31f52e3a0e4
Francis Pires de Carvalho <francisp@LFP.LOCAL>
2019-05-06 18:57:56 -0300

    u

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ source ../deploy-envs.sh
 export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
 # Build process
-docker build -t 770676894615.dkr.ecr.us-east-2.amazonaws.com/redis-geo:latest ../
+docker build -t $IMAGE_NAME ../
 docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
